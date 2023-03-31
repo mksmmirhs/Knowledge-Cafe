69e22cf371eb91aadf8075d9aaacf3f2e1e1e236
Merajul Islam <meraj.exe@gmail.com>
2023-03-31 09:05:00 +0600

    toast complete

--- src/Components/Blogs/Blogs.jsx
+++ src/Components/Blogs/Blogs.jsx
@@ -2,20 +2,31 @@ import React, { useEffect, useState } from 'react';
 import Blog from '../Blog/Blog';
 import Bookmarks from '../Bookmarks/bookmarks';
 import './Blogs.css';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 const Blogs = () => {
-  const [blog, setBlog] = useState([]);
+  const [blogs, setBlogs] = useState([]);
+  const [bookmarks, setBookmarks] = useState([]);
   useEffect(() => {
     fetch('data.json')
       .then(res => res.json())
-      .then(data => setBlog(data));
+      .then(data => setBlogs(data));
   }, []);
+  const notify = () => toast('Already bookmarked');
   const bookmarkHandler = id => {
-    console.log(id);
+    const exist = bookmarks.find(bookmark => bookmark.id === id);
+    if (!exist) {
+      const bookmarkFind = blogs.find(blog => blog.id === id);
+      const tempBookmark = [...bookmarks, bookmarkFind];
+      setBookmarks(tempBookmark);
+    } else {
+      notify();
+    }
   };
   return (
     <div className="blogs-container">
       <div className="blogs">
-        {blog.map(blog => (
+        {blogs.map(blog => (
           <Blog
             key={blog.id}
             blog={blog}
@@ -24,7 +35,19 @@ const Blogs = () => {
         ))}
       </div>
       <div className="bookmarks">
-        <Bookmarks></Bookmarks>
+        <Bookmarks bookmarks={bookmarks}></Bookmarks>
+        <ToastContainer
+          position="top-center"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="dark"
+        />
       </div>
     </div>
   );
--- src/Components/Bookmarks/Bookmarks.jsx
+++ src/Components/Bookmarks/Bookmarks.jsx
@@ -2,14 +2,15 @@ import React from 'react';
 import Bookmark from '../Bookmark/Bookmark';
 import './Bookmarks.css';
 
-const Bookmarks = () => {
+const Bookmarks = props => {
+  const { bookmarks } = props;
   return (
     <div className="Bookmarks">
       <div className="total-read">
         <p>Spent time on read : 177 min</p>
       </div>
       <div className="bookmark">
-        <h1>Bookmarked Blogs : 8</h1>
+        <h1>Bookmarked Blogs : {bookmarks.length}</h1>
         <div className="blog-titles">
           <Bookmark></Bookmark>
           <Bookmark></Bookmark>
